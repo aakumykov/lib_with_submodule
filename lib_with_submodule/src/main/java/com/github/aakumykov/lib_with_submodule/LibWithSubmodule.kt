71318324904372c1d5fea9ff7feade26d2634a29
aakumykov <aakumykov@yandex.ru>
2026-05-22 18:44:11 +0300

    Переставил местами блоки условного подключения, задействовал TestClass2; v0.0.6

--- lib_with_submodule/src/main/java/com/github/aakumykov/lib_with_submodule/LibWithSubmodule.kt
+++ lib_with_submodule/src/main/java/com/github/aakumykov/lib_with_submodule/LibWithSubmodule.kt
@@ -2,11 +2,13 @@ package com.github.aakumykov.lib_with_submodule
 
 import android.util.Log
 import com.github.aakumykov.test_android_module.TestClass1
+import com.github.aakumykov.test_android_module.TestClass2
 
 class LibWithSubmodule {
     fun work() {
         Log.d(TAG, "work()")
         TestClass1().work()
+        TestClass2().work()
     }
     companion object {
         val TAG: String = LibWithSubmodule::class.java.simpleName
