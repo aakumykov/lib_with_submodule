4f98bf5cbd875b0a8ce110ba58b985fc8dbe63c3
aakumykov <aakumykov@yandex.ru>
2026-05-22 15:59:00 +0300

    Подключил (под)модуль test_android_module к модулю lib_with_submodule, задействовал класс оттуда.

--- lib_with_submodule/src/main/java/com/github/aakumykov/lib_with_submodule/LibWithSubmodule.kt
+++ lib_with_submodule/src/main/java/com/github/aakumykov/lib_with_submodule/LibWithSubmodule.kt
@@ -1,10 +1,12 @@
 package com.github.aakumykov.lib_with_submodule
 
 import android.util.Log
+import com.github.aakumykov.test_android_module.TestClass1
 
 class LibWithSubmodule {
     fun work() {
         Log.d(TAG, "work()")
+        TestClass1().work()
     }
     companion object {
         val TAG: String = LibWithSubmodule::class.java.simpleName
