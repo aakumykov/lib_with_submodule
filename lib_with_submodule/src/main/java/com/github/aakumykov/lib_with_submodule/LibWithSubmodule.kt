aebedf305350acc32dc500d77b276bdeb06f5e4b
aakumykov <aakumykov@yandex.ru>
2026-05-25 11:10:27 +0300

    Задействовал Qwerty в lib_with_submodule; v0.0.8

--- lib_with_submodule/src/main/java/com/github/aakumykov/lib_with_submodule/LibWithSubmodule.kt
+++ lib_with_submodule/src/main/java/com/github/aakumykov/lib_with_submodule/LibWithSubmodule.kt
@@ -1,6 +1,8 @@
 package com.github.aakumykov.lib_with_submodule
 
+import android.text.method.QwertyKeyListener
 import android.util.Log
+import com.github.aakumykov.qwerty_test_lib.Qwerty
 import com.github.aakumykov.test_android_module.TestClass1
 import com.github.aakumykov.test_android_module.TestClass2
 
@@ -9,6 +11,7 @@ class LibWithSubmodule {
         Log.d(TAG, "work()")
         TestClass1().work()
         TestClass2().work()
+        Qwerty().work()
     }
     companion object {
         val TAG: String = LibWithSubmodule::class.java.simpleName
