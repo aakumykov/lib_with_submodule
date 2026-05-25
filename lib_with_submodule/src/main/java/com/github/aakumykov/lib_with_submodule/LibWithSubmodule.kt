17c4ba085132a826dfba1e8bd214d5179305c061
aakumykov <aakumykov@yandex.ru>
2026-05-25 18:37:11 +0300

    Подмодуль copy_between_streams_with_speed, перешёл в этом модуле на встроенный kotlin; v0.0.10

--- lib_with_submodule/src/main/java/com/github/aakumykov/lib_with_submodule/LibWithSubmodule.kt
+++ lib_with_submodule/src/main/java/com/github/aakumykov/lib_with_submodule/LibWithSubmodule.kt
@@ -1,17 +1,28 @@
 package com.github.aakumykov.lib_with_submodule
 
-import android.text.method.QwertyKeyListener
 import android.util.Log
+import com.github.aakumykov.copy_between_streams_with_speed.copyBetweenStreamsWithSpeed
 import com.github.aakumykov.qwerty_test_lib.Qwerty
 import com.github.aakumykov.test_android_module.TestClass1
 import com.github.aakumykov.test_android_module.TestClass2
+import java.io.File
+import java.util.UUID
 
 class LibWithSubmodule {
+
+    private val randomId: String get() = UUID.randomUUID().toString()
+
     fun work() {
         Log.d(TAG, "work()")
         TestClass1().work()
         TestClass2().work()
         Qwerty().work()
+
+        copyBetweenStreamsWithSpeed(
+            File.createTempFile(randomId, randomId).inputStream(),
+            File.createTempFile(randomId, randomId).outputStream(),
+            printDebug = true
+        )
     }
     companion object {
         val TAG: String = LibWithSubmodule::class.java.simpleName
